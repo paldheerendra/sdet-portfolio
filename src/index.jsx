c86efa442474cb3199fc38a3869fff2d61721dab
paldheerendra <dheerendras06@hotmail.com>
2025-08-16 13:12:35 +0530

    moved app.jsx to /src to resove error

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";  // Tailwind styles
+import "./index.css";
+// Tailwind styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
