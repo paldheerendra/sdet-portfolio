148f65897805964e0723ded9d08d771a84c3bfcc
paldheerendra <dheerendras06@hotmail.com>
2025-08-16 13:23:21 +0530

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// This is a Tailwind CSS configuration file that specifies the content sources
